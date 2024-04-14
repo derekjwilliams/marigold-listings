6ce0a6636a1ede468c80a87e9faf1fe6d1edb2c1
derek williams <derek61@gmail.com>
2024-04-14 07:59:39 -0600

    set object-fit to be cover

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
   // Configure `pageExtensions`` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
+  images: {
+    domains: ['s3.amazonaws.com'],
+  },
 }
 
 const withMDX = createMDX({
