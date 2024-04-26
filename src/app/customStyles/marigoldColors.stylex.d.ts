345099f3f2cb5d8c35e0c16f2285db9fee4d32f5
derek williams <derek61@gmail.com>
2024-04-26 11:49:19 -0600

    update styles

---
+++ src/app/customStyles/marigoldColors.stylex.d.ts
@@ -0,0 +1,11 @@
+import type { VarGroup } from '@stylexjs/stylex/lib/StyleXTypes'
+
+type TColors = Readonly<{
+  flowerYellow: string
+  flowerGold: string
+  flowerRed: string
+  environmentBrown: string
+  leaf: string
+  pansy: string
+}>
+export declare const marigoldColors: VarGroup<TColors>
---
+++ src/app/customStyles/marigoldColors.stylex.js
@@ -0,0 +1,9 @@
+import { defineVars } from '@stylexjs/stylex'
+export const marigoldColors = defineVars({
+  flowerYellow: 'rgb(255 213 95)',
+  flowerGold: 'rgb(255 194 0)',
+  flowerRed: 'rgb(247 70 0)',
+  environmentBrown: 'rgb(214 122 0)',
+  leaf: 'rgb(0 81 69)',
+  pansy: 'rgb(168 0 173)',
+})
